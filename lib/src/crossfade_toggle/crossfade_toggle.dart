8977403f1bfc5d7e7d9db5a996d0a48f53260171
Joey Boerwinkel <joey@iconica.nl>
2023-12-27 16:16:30 +0100

    feat(crossfade): add crossfade boolean toggle extension

---
+++ lib/src/crossfade_toggle/crossfade_toggle.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/widgets.dart';
+
+extension CrossFadeStateBool on CrossFadeState {
+  /// Returns the other crossfade state
+  ///
+  /// created as an optimisation on
+  /// ```dart
+  /// bool ? CrossFadeState.showFirst : CrossFadeState.showSecond
+  /// ```
+  ///
+  /// If you want to have it toggle based on a boolean condition, you can
+  /// do the following:
+  ///
+  /// ```dart
+  /// // define the state
+  /// bool showSecond = true
+  ///
+  ///
+  /// // in the build
+  /// // ...
+  /// AnimatedCrossFade(
+  ///   ...
+  ///   crossFadeState: CrossFadeState.showFirst.toggle(toggle: showSecond,)
+  ///   ...
+  /// )
+  /// // ...
+  /// ```
+  CrossFadeState toggle({bool toggle = true}) {
+    if (!toggle) {
+      return this;
+    }
+    return switch (this) {
+      CrossFadeState.showFirst => CrossFadeState.showSecond,
+      CrossFadeState.showSecond => CrossFadeState.showFirst,
+    };
+  }
+}
